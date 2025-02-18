d89e3fedfad67b130928ee6f86ae7043cad33877
h41ku <h41ku.c0de@ya.ru>
2025-02-18 12:01:31 +0300

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,81 @@
+// common
+
+export type QueryResponse = {
+    status: number,
+    data: any | undefined,
+    error: any | undefined
+};
+
+export type QueryFn = (options: any | undefined) => QueryResponse;
+
+export type PluginObtainTokenOptions = {
+    error: string | undefined,
+    success: string | undefined,
+    acceptState: (state: string | undefined) => Promise<boolean>,
+    acceptScope: (scope: string | undefined) => Promise<boolean>
+};
+
+export type PluginRefreshTokenOptions = {
+    reservedTime: number | undefined,
+    header: string | undefined
+};
+
+export type PluginAuthenticatedUserOptions = {
+    method: string | undefined,
+    url: string | undefined,
+    expectedStatus: number | undefined,
+    selector: (data: any) => any
+};
+
+export type PluginLogoutOptions = {
+    method: string | undefined,
+    url: string | undefined
+};
+
+export type PluginLoginOptions = {
+    prepareState: () => Promise<string>
+};
+
+export type PluginsOptions = {
+    login: PluginLoginOptions | undefined,
+    obtainToken: PluginObtainTokenOptions | undefined,
+    refreshToken: PluginRefreshTokenOptions | undefined,
+    authenticatedUser: PluginAuthenticatedUserOptions | QueryFn | undefined,
+    logout: PluginLogoutOptions | QueryFn | undefined
+};
+
+export type OAUth2ClientOptions = {
+    provider: string | undefined,
+    credentials: {
+        clientId: string,
+        clientSecret: string,
+        redirectUri: string,
+        scope: string
+    },
+    endpoints: {
+        authorize: string,
+        getAccessToken: string,
+        refreshToken: string
+    },
+    plugins: PluginsOptions | undefined
+};
+
+// client/express
+
+export type ExpressSyncController = (request: any, response: any) => void;
+export type ExpressAsyncController = (request: any, response: any) => Promise<void>;
+export type ExpressAsyncMiddleware = (request: any, response: any, next: any) => Promise<void>;
+
+export type ExpressOAuth2Client = {
+    controllers: {
+        login: ExpressSyncController,
+        logout: ExpressAsyncController,
+        obtainToken: ExpressAsyncController
+    },
+    middlewares: {
+        refreshToken: ExpressAsyncMiddleware,
+        authenticatedUser: ExpressAsyncMiddleware
+    }
+};
+
+export declare function expressOAuth2Client(options: OAUth2ClientOptions | undefined): ExpressOAuth2Client;
