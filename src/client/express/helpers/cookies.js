0e1975a73e2b07819439fa1b4583946db0ab5155
h41ku <h41ku.c0de@ya.ru>
2025-02-18 11:51:26 +0300

    Set value or clear, if it is empty

--- src/client/express/helpers/cookies.js
+++ src/client/express/helpers/cookies.js
@@ -2,4 +2,11 @@ const cookieOptions = { httpOnly: true, sameSite: 'strict' }
 const clearCookie = (response, key) => response.clearCookie(key, cookieOptions)
 export const clearCookies = (response, keys) => keys.forEach(key => clearCookie(response, key))
 const setCookie = (response, key, value) => response.cookie(key, value, cookieOptions)
-export const setCookies = (response, kv) => Object.keys(kv).map(key => setCookie(response, key, kv[key]))
+export const setCookies = (response, kv) => Object.keys(kv).map(key => {
+    const value = kv[key]
+    if (value === undefined || value === null) {
+        clearCookie(response, key)
+    } else {
+        setCookie(response, key, value)
+    }
+})
