18725e00bfb53e114df253fd020e2fc6276ac075
h41ku <h41ku.c0de@ya.ru>
2025-02-18 11:56:04 +0300

    Add state generator

--- src/client/express/controllers/login.js
+++ src/client/express/controllers/login.js
@@ -1,11 +1,17 @@
-export default (options = {}) => (request, response) => {
+export default (options = {}) => async (request, response) => {
     const {
         clientId: client_id,
         redirectUri: redirect_uri,
         scope
     } = (options?.credentials || {})
+    const {
+        prepareState
+    } = {
+        prepareState: async () => Date.now(),
+        ...(options?.plugins?.login || {})
+    }
     const { authorize } = (options?.endpoints || {})
-    const state = Date.now() // TODO
+    const state = await prepareState()
     const url = `${authorize}?${new URLSearchParams({ response_type: 'code', client_id, redirect_uri, scope, state })}`
     response.status(302)
     response.set({ Location: url })
