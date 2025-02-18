86b60b9f5f85825e3b2161074848970cfdd41c2a
h41ku <h41ku.c0de@ya.ru>
2025-02-18 12:01:07 +0300

    Add support of provider

--- src/client/express/middlewares/refreshToken.js
+++ src/client/express/middlewares/refreshToken.js
@@ -1,12 +1,17 @@
 import query from '../helpers/query.js'
 import extractToken from '../helpers/extractToken.js'
 import { setCookies, clearCookies } from '../helpers/cookies.js'
+import unexpectedProviderError from '../errors/unexpectedProvider.js'
 
-const createContext = (request, accessToken) => {
-    request.oauth2 = { accessToken }
+const createContext = (request, provider, accessToken) => {
+    request.oauth2 = {
+        ...(provider ? { provider } : {}),
+        accessToken
+    }
 }
 
 export default (options = {}) => {
+    const { provider: providerExpected } = options
     const {
         clientId: client_id,
         clientSecret: client_secret
@@ -21,14 +26,13 @@ export default (options = {}) => {
         ...(options?.plugins?.refreshToken)
     }
     return async (request, response, next) => {
-        const { refresh_token, expires_at } = request.cookies
-        if (!refresh_token) {
+        const { provider, refresh_token, expires_at } = request.cookies
+        if (provider !== providerExpected) {
+            unexpectedProviderError(provider, providerExpected)
+        } else if (!refresh_token) {
             response.status(401).end()
         } else {
             const access_token = extractToken(request)
-// console.log('refreshToken()', request.originalUrl, { refresh_token, expires_at, access_token })
-// response.status(502).end()
-// return;
             if (!access_token || (parseInt(expires_at) - reservedTime) * 1000 <= Date.now()) {
                 const { status, data: token } = await query({
                     url: refreshToken,
@@ -40,31 +44,31 @@ export default (options = {}) => {
                         client_secret
                     })
                 })
-// console.log('refreshToken()', request.originalUrl, 'new token', { status, token })
                 if (status === 0) {
                     response.status(503).end()
                 } else if (status === 200) {
                     const { access_token, refresh_token, expires_in } = token
                     const expires_at = Math.floor(Date.now() / 1000 + expires_in)
                     setCookies(response, {
+                        provider,
                         refresh_token,
                         expires_at
                     })
                     if (header) {
                         response.set({ [header]: access_token })
                     }
-                    createContext(request, access_token)
+                    createContext(request, provider, access_token)
                     next()
                 } else {
                     clearCookies(response, [
+                        'provider',
                         'refresh_token',
                         'expires_at'
                     ])
                     response.status(401).end()
                 }
             } else {
-// console.log('refreshToken()', request.originalUrl, 'old token', { access_token })
-                createContext(request, access_token)
+                createContext(request, provider, access_token)
                 next()
             }
         }
