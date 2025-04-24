f21ccc694fd97ac0c619d85e82764d06fda1c071
h41ku <h41ku.c0de@ya.ru>
2025-04-24 14:56:43 +0300

    Add extensible options

--- client/express/helpers/cookies.js
+++ client/express/helpers/cookies.js
@@ -1,12 +1,34 @@
-const cookieOptions = { httpOnly: true, sameSite: 'strict' }
-const clearCookie = (response, key) => response.clearCookie(key, cookieOptions)
-export const clearCookies = (response, keys) => keys.forEach(key => clearCookie(response, key))
-const setCookie = (response, key, value) => response.cookie(key, value, cookieOptions)
-export const setCookies = (response, kv) => Object.keys(kv).map(key => {
-    const value = kv[key]
-    if (value === undefined || value === null) {
-        clearCookie(response, key)
-    } else {
-        setCookie(response, key, value)
-    }
-})
+export const defaultOptions = { httpOnly: true, sameSite: 'strict' }
+
+const setCookie = (response, key, value, cookieOptions) => response.cookie(key, value, cookieOptions)
+const clearCookie = (response, key, cookieOptions) => response.clearCookie(key, cookieOptions)
+
+export const getCookies = (request, keys, options = {}) => {
+    const { prefix } = { ...defaultOptions, ...options }
+    return keys.reduce((kv, originalKey) => {
+        const key = prefix === undefined ? originalKey : `${prefix}${originalKey}`
+        kv[originalKey] = request.cookies[key]
+        return kv
+    }, {})
+}
+
+export const setCookies = (response, kv, options = {}) => {
+    const { prefix, ...cookieOptions } = { ...defaultOptions, ...options }
+    Object.keys(kv).map(originalKey => {
+        const value = kv[originalKey]
+        const key = prefix === undefined ? originalKey : `${prefix}${originalKey}`
+        if (value === undefined || value === null) {
+            clearCookie(response, key, cookieOptions)
+        } else {
+            setCookie(response, key, value, cookieOptions)
+        }
+    })
+}
+
+export const clearCookies = (response, keys, options = {}) => {
+    const { prefix, ...cookieOptions } = { ...defaultOptions, ...options }
+    keys.forEach(originalKey => {
+        const key = prefix === undefined ? originalKey : `${prefix}${originalKey}`
+        clearCookie(response, key, cookieOptions)
+    })
+}
